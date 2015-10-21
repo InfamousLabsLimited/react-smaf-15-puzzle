12c1446446198ebe75e27c4abad32d934f5b09aa
af7 <aris@likno.com>
2015-10-21 12:47:37 +0300

    Send the imported 'Smaf' library object to the root React component through its 'props'

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import App from './components/app'
 import Smaf from './smaf';
 
-ReactDOM.render(<App />, document.getElementById('main'));
+ReactDOM.render(<App Smaf={Smaf}/>, document.getElementById('main'));
